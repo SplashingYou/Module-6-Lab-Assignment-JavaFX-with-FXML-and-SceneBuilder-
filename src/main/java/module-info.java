a07db5ec1b1a10dd0aeb116a2dd12146ce60af26
SplashingYou <117105654+SplashingYou@users.noreply.github.com>
2023-10-11 23:17:27 -0400

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.colorpickerhw {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.colorpickerhw to javafx.fxml;
-    exports com.example.colorpickerhw;
-}
